88f5840423151bef4ca363b0f115c263ac9e773e
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 02:39:29 +0700

    KS-2-3 gotcha time

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,2 +1,14 @@
 package org.example.lesson2
 
+fun main (){
+    val startHours = 9
+    val startMinsOfHour = 39
+    val roadTimeMins = 457
+
+    val startMinutes = startHours * 60 + startMinsOfHour
+    val finishTimeInMins = startMinutes + roadTimeMins
+    val finishTimeHours: Int = finishTimeInMins / 60
+    val finishTimeMinsOfHour: Int = finishTimeInMins % 60
+
+    println(String.format("%02d:%02d", finishTimeHours, finishTimeMinsOfHour))
+}
