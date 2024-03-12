e9ffe7e6050c0792c28d874b124d3d90c3eb9c54
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 02:24:13 +0700

    KS-2-2 all numbers calculated

--- src/main/kotlin/lesson2/lesson2_task2.kt
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -1,2 +1,16 @@
 package org.example.lesson2
 
+fun main(){
+    val employees = 50
+    val emplSalary = 30000
+    val interns = 30
+    val interSalary = 20000
+
+    val sumEmplPayments = 50 * 30000
+    val sumAllPayments = sumEmplPayments + (30 * 20000)
+    val averageSalary = sumAllPayments / (50+30)
+
+    println(sumEmplPayments.toInt())
+    println(sumAllPayments.toInt())
+    println(averageSalary.toInt())
+}
