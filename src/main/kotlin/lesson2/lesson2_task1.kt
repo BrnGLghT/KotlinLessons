b68ba9f72c8de545d7844f0e6c0a1533155d226c
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 02:13:10 +0700

    KS-2-1 avg number calculated

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson2
 
 fun main(){
-    val avgGrade: Double = (3.0 +4.0 +3.0+5.0) / 4.0
+    val avgGrade: Double = (3.0 + 4.0 + 3.0 + 5.0) / 4.0
     val formatAvgGrade = String.format("%.2f", avgGrade)
     println(formatAvgGrade)
 }
