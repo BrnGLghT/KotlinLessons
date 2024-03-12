7e0931d641314162288a1efc9c1b2875f3d897d1
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 02:46:39 +0700

    KS-2-4 bonus ore calculated

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,2 +1,11 @@
 package org.example.lesson2
 
+const val BUFF_PERCENT = 20
+
+fun main (){
+    val crystalOre = 7
+    val ironOre = 11
+
+    println("Дополнительно добытая кристаллическая руда = ${(crystalOre * BUFF_PERCENT) / 100}")
+    println("Дополнительно добытая металлическая руда = ${(ironOre * BUFF_PERCENT) / 100}")
+}
