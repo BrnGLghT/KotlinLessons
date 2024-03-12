ec51ce3505acd66993dc67342374b583a8397cd4
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 01:00:17 +0700

    KS-1-5 standard time forman

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,2 +1,12 @@
 package org.example.lesson1
+const val secondsInHour = 3600
+const val secondsInMinute = 60
+fun main(){
+    val seconds = 6480
+    val hours = (seconds / secondsInHour)
+    val hoursRestSecs = (seconds - secondsInHour)
+    val minutes = (hoursRestSecs / secondsInMinute)
+    val minutesRestSecs = (seconds % minutes)
 
+    println(String.format("%02d:%02d:%02d", hours, minutes, minutesRestSecs))
+}
