0fbe0be5feaa93d5f721c10a8a6e85d31e39505a
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 00:56:49 +0700

    KS-1-5 added more space and rename consts

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,11 +1,14 @@
 package org.example.lesson1
-const val secondsInHour = 3600
-const val secondsInMinute = 60
-fun main(){
+
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+
+
+fun main() {
     val seconds = 6480
-    val hours = (seconds / secondsInHour)
-    val hoursRestSecs = (seconds - secondsInHour)
-    val minutes = (hoursRestSecs / secondsInMinute)
+    val hours = (seconds / SECONDS_IN_HOUR)
+    val hoursRestSecs = (seconds - SECONDS_IN_HOUR)
+    val minutes = (hoursRestSecs / SECONDS_IN_MINUTE)
     val minutesRestSecs = (seconds % minutes)
 
     println(String.format("%02d:%02d:%02d", hours, minutes, minutesRestSecs))
