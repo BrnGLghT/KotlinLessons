fabc7f90f5a47a3685b82e58405ab30a1e6a22da
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-13 22:09:40 +0700

    KS-3-2 marriage problems

--- src/main/kotlin/lesson3/lesson3_task2.kt
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -1,2 +1,12 @@
 package org.example.lesson3
 
+fun main() {
+    val firstName = "Татьяна"
+    var secondName = "Андреева"
+    val thirdName = "Сергеевна"
+    var age = 20
+    println("$secondName $firstName $thirdName, $age")
+    age = 22
+    secondName = "Сидорова"
+    println("$secondName $firstName $thirdName, $age")
+}
