6ea98e22d16790524315926e3ef8b338e40039c4
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 23:20:00 +0700

    KS-4-1 added solution

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,2 +1,10 @@
 package org.example.lesson4
 
+const val ALL_TABLES = 13
+
+fun main() {
+    val todayReserv = 13
+    val tomorrowReserv = 9
+
+    println("Доступность столиков на сегодня: ${todayReserv != ALL_TABLES}\nДоступность столиков на завтра: ${tomorrowReserv != ALL_TABLES}")
+}
