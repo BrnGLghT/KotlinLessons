d568d39ba0a2496dea6fca05319d81f54da3b215
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 23:45:58 +0700

    KS-4-3 time check

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,2 +1,11 @@
 package org.example.lesson4
 
+fun main() {
+
+    val isSunny = true
+    val isRoofOpen = true
+    val airHumidity = 20
+    val season = "Зима"
+
+    println("Благоприятные сейчас условия для бобовых? ${isSunny && isRoofOpen && airHumidity == 20 && season != "Зима"}")
+}
