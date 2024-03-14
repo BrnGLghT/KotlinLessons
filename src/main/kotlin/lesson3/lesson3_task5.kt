3d365c8d5a15ab30608ab571788a48b9b94d143d
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 23:26:48 +0700

    KS-3-5 add solution

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -1,2 +1,15 @@
 package org.example.lesson3
 
+fun main() {
+    val playerMove = "D2-D4;0"
+
+    val movesList = playerMove.split(";")
+    val move = movesList[0].split("-")
+    val start = move[0]
+    val finish = move[1]
+    val moveNum = movesList[1]
+
+    println("Откуда: $start")
+    println("Куда: $finish")
+    println("Номер хода: $moveNum")
+}
