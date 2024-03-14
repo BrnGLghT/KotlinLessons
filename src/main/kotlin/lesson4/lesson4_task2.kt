4a51da73b937b0da931cf335441a3ff4fcb680e3
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 23:43:21 +0700

    KS-4-2

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,2 +1,13 @@
 package org.example.lesson4
 
+fun main() {
+    //Average масса больше 35 и меньше 100, общем меньше 100
+    val firstPackWeight = 20
+    val firstPackVolume = 80
+
+    val secondPackWeight = 50
+    val secondPackVolume = 100
+
+    println("Груз весом $firstPackWeight кг и объемом $firstPackVolume соотвествует категории 'Average':${firstPackWeight <= 100 && firstPackWeight >= 35 && firstPackVolume <=100}\n" +
+            "руз весом $secondPackWeight кг и объемом $secondPackVolume соотвествует категории 'Average':${secondPackWeight <= 100 && secondPackWeight >= 35 && secondPackVolume <=100}")
+}
