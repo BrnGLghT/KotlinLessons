6a96eeac701eeb25b8d9db229f97c2b1b176f913
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-14 23:54:55 +0700

    KS-4-4 exercises days

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -1,2 +1,14 @@
 package org.example.lesson4
 
+fun main() {
+    val day = 5
+    val exercisesForArmsAndAbs = day % 2 == 0
+    val exercisesForLegsAndBack = day % 2 != 0
+
+    println("""
+        Упражнения для рук:       $exercisesForArmsAndAbs
+        Упражнения для ног:        $exercisesForLegsAndBack
+        Упражнения для спины:      $exercisesForLegsAndBack
+        Упражнения для пресса:    $exercisesForArmsAndAbs
+    """.trimIndent())
+}
