ffeb453a3f496f4719aa350be5e354327ececf7f
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-15 13:02:22 +0700

    KS-2-4 changed vars

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -3,8 +3,8 @@ package org.example.lesson2
 fun main (){
     val crystalOre = 7
     val ironOre = 11
-    val buffPercent = 20
     val divider = 100
+    val buffPercent = 20
 
     println("Дополнительно добытая кристаллическая руда = ${(crystalOre * buffPercent) / divider}")
     println("Дополнительно добытая металлическая руда = ${(ironOre * buffPercent) / divider}")
