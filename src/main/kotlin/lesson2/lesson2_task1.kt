51e44b0929bd3e87b843c6c25a78c8b373af8858
Max <58424288+BrnGLghT@users.noreply.github.com>
2024-03-15 12:54:19 +0700

    Revert "Ks 2 1"

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,7 +1,2 @@
 package org.example.lesson2
 
-fun main(){
-    val avgGrade = ((3 + 4 + 3 + 5).toFloat() / 4)
-    val formatAvgGrade = String.format("%.2f", avgGrade)
-    println(formatAvgGrade)
-}
