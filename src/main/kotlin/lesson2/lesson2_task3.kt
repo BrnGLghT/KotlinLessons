1fb0705d378a83c8fd2367aa090720b1b88770d5
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-15 12:58:37 +0700

    KS-2-3 fixed magic num

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,14 +1,16 @@
 package org.example.lesson2
 
+const val MINS_IN_HOUR = 60
+
 fun main (){
     val startHours = 9
     val startMinsOfHour = 39
     val roadTimeMins = 457
 
-    val startMinutes = startHours * 60 + startMinsOfHour
+    val startMinutes = startHours * MINS_IN_HOUR + startMinsOfHour
     val finishTimeInMins = startMinutes + roadTimeMins
-    val finishTimeHours: Int = finishTimeInMins / 60
-    val finishTimeMinsOfHour: Int = finishTimeInMins % 60
+    val finishTimeHours: Int = finishTimeInMins / MINS_IN_HOUR
+    val finishTimeMinsOfHour: Int = finishTimeInMins % MINS_IN_HOUR
 
     println(String.format("%02d:%02d", finishTimeHours, finishTimeMinsOfHour))
 }
