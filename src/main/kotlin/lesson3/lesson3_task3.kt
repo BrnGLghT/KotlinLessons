567f55bdac176ba7dd95ff540f147bcb2da511e3
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-16 22:02:48 +0700

    KS-3-3 formated code

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -2,7 +2,7 @@ package org.example.lesson3
 
 fun main() {
     val number = 6
-    for (i in 1..10){
-        println("$number * $i = ${number*i}")
+    for (i in 1..10) {
+        println("$number * $i = ${number * i}")
     }
 }
