945856387d2435c4843da01db583252c73a52ac4
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-17 13:48:44 +0700

    KS-4-2 add avg consts

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,5 +1,9 @@
 package org.example.lesson4
 
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
+
 fun main() {
     //Average масса больше 35 и меньше 100, общем меньше 100
     val firstPackWeight = 20
@@ -8,6 +12,10 @@ fun main() {
     val secondPackWeight = 50
     val secondPackVolume = 100
 
-    println("Груз весом $firstPackWeight кг и объемом $firstPackVolume соотвествует категории 'Average':${firstPackWeight <= 100 && firstPackWeight >= 35 && firstPackVolume <=100}\n" +
-            "руз весом $secondPackWeight кг и объемом $secondPackVolume соотвествует категории 'Average':${secondPackWeight <= 100 && secondPackWeight >= 35 && secondPackVolume <=100}")
+    println(
+        "Груз весом $firstPackWeight кг и объемом $firstPackVolume соотвествует категории" +
+                " 'Average':${(firstPackWeight <= MAX_WEIGHT_AVERAGE) && (firstPackWeight >= MIN_WEIGHT_AVERAGE) && (firstPackVolume <= MAX_VOLUME_AVERAGE)}\n" +
+                "Груз весом $secondPackWeight кг и объемом $secondPackVolume соотвествует категории" +
+                " 'Average':${(secondPackWeight <= MAX_WEIGHT_AVERAGE) && (secondPackWeight >= MIN_WEIGHT_AVERAGE) && (secondPackVolume <= MAX_VOLUME_AVERAGE)}"
+    )
 }
