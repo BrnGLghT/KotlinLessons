d94cd670a4bc539017bd84ba9d6476c7bf6748ed
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-17 13:58:28 +0700

    KS-4-3 added consts and comparison

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,11 +1,20 @@
 package org.example.lesson4
 
+const val WEATHER_SUNNY = true
+const val ROOF_OPENED = true
+const val AIR_HUMIDITY_PERFECT = 20
+const val SEASON_SPRING = "Весна"
+const val SEASON_SUMMER = "Лето"
+const val SEASON_FALL = "Осень"
+
+
 fun main() {
 
-    val isSunny = true
-    val isRoofOpen = true
-    val airHumidity = 20
-    val season = "Зима"
+    val isSunnyToday = true
+    val isRoofOpenToday = true
+    val airHumidityToday = 20
+    val seasonToday = "Зима"
 
-    println("Благоприятные сейчас условия для бобовых? ${isSunny && isRoofOpen && airHumidity == 20 && season != "Зима"}")
+    println("Благоприятные сейчас условия для бобовых? ${isSunnyToday == WEATHER_SUNNY && isRoofOpenToday == ROOF_OPENED &&
+    airHumidityToday == AIR_HUMIDITY_PERFECT && (seasonToday == SEASON_SPRING || seasonToday == SEASON_SUMMER || seasonToday == SEASON_FALL)}")
 }
