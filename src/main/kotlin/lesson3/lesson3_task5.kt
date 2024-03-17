935bf447d2c0463c372efe0d00a4629d1f200723
DESKTOP-968KHVB\BurninGLighT <mamyshev.m@azurgames.com>
2024-03-17 13:44:00 +0700

    KS-3-5 merged two splits into one

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,11 +3,10 @@ package org.example.lesson3
 fun main() {
     val playerMove = "D2-D4;0"
 
-    val movesList = playerMove.split(";")
-    val move = movesList[0].split("-")
-    val start = move[0]
-    val finish = move[1]
-    val moveNum = movesList[1]
+    val splitPlayerMoveString = playerMove.split("-", ";")
+    val start = splitPlayerMoveString[0]
+    val finish = splitPlayerMoveString[1]
+    val moveNum = splitPlayerMoveString[2]
 
     println("Откуда: $start")
     println("Куда: $finish")
